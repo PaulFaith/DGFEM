5ecd6dbd741e28110723e40e9d658ace3500dd38
PaulFaith <62285801+PaulFaith@users.noreply.github.com>
2020-10-29 03:01:47 +0100

    Update mesh.py

--- src/DGFEM/mesh.py
+++ src/DGFEM/mesh.py
@@ -3,5 +3,20 @@ import copy
 import math
 
 class Mesh:
-
-    def __init__(self, coordinates, elements, grid):
+    #Constructor.       
+    def __init__(self, xmin, xmax, K):
+        self.xmin = xmin
+        self.xmax = xmax
+        self.K = K
+    
+    #Generate node cordinates.
+    def coordinates(self):
+        return np.linspace(self.xmin, self.xmax, self.K+1, endpoint=True)
+    
+    #Read element to node connectivity.
+    def EToV(self):
+        self.EToV = np.zeros((self.K,2))
+        for k in range(self.K)
+        self.EToV[k,1]=k+1
+        self.EToV[k,2]=k+2
+        return self.EToV
