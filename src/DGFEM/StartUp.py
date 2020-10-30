f1bbf810edfd0c4aa93f205b689012d7fca5af41
PaulFaith <62285801+PaulFaith@users.noreply.github.com>
2020-10-30 18:20:05 +0100

    Create StartUp.py
    Preparing the body.

---
+++ src/DGFEM/StartUp.py
@@ -0,0 +1,21 @@
+#Puropose: Script for building operators, grid, metric and connectivity for 1d solver.
+
+#CONSTANTS AND VARIABLES ON THE EXAMPLE WAS GLOBALS1D.
+
+#Compute basic Legendre Gauss Lobato grid.
+
+#Build reference element matrices.
+
+#Create surface integral terms.
+
+#Build coordinates of all the nodes.
+
+#Calculate geometric factors.
+
+#Compute masks for edge nodes.
+
+#Build surface normals and inverse metric at surface.
+
+#Build connectivity matrix.
+
+#Build connectivity maps.
