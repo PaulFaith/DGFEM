0ccbc1ed7122912a27a6f017f3603c47a7255039
PaulFaith <62285801+PaulFaith@users.noreply.github.com>
2020-11-03 08:07:08 +0100

    Update StartUp.py
    New changes

--- src/DGFEM/StartUp.py
+++ src/DGFEM/StartUp.py
@@ -5,10 +5,30 @@ import numpy as np
 class Galerkin:
   #CONSTANTS AND VARIABLES ON THE EXAMPLE WAS GLOBALS1D.
   #Constructor.       
-  def __init__():
-  #Compute basic Legendre Gauss Lobato grid.
-  def gridLGL(self):
-    Pass
+  def __init__(NODETOL, Np, Nfp, Nfaces):
+    self.NODETOL = NODETOL
+    self.Np = Np
+    self.Nfp = Nfp
+    self.Nfaces = Nfaces
+  
+  #Compute the basic Legendre Gauss Lobato grid.
+  def jacobiGridLGL(self, alpha, beta, N):
+    self.alpha = alpha
+    self.beta = beta
+    self.N = N
+    self._x = np.zeros(self.N+1,1)
+    if (N == 1):
+      self._x(0) = -1.
+      self._x(1) = 1.
+      return
+    [self._xint, self._w] = JacobiGQ(self.alpha + 1, self.beta + 1, self.N - 2)
+    self._x = np.append(-1., np.transpose(self._xint), 1.)
+  
+  def jacobiGQ(self, a, b, N):
+    self.a = a
+    self.b = b
+    self.N = N
+    
   #Build reference element matrices.
   def matrixElements(self):
     Pass
