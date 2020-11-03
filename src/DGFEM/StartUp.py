a561b19056d386c6401285a016486e3f82eb421c
PaulFaith <62285801+PaulFaith@users.noreply.github.com>
2020-11-03 04:06:19 +0100

    Update StartUp.py
    Naming the important methods for the main module.

--- src/DGFEM/StartUp.py
+++ src/DGFEM/StartUp.py
@@ -1,21 +1,35 @@
 #Puropose: Script for building operators, grid, metric and connectivity for 1d solver.
 
-#CONSTANTS AND VARIABLES ON THE EXAMPLE WAS GLOBALS1D.
+import numpy as np
 
-#Compute basic Legendre Gauss Lobato grid.
-
-#Build reference element matrices.
-
-#Create surface integral terms.
-
-#Build coordinates of all the nodes.
-
-#Calculate geometric factors.
-
-#Compute masks for edge nodes.
-
-#Build surface normals and inverse metric at surface.
-
-#Build connectivity matrix.
-
-#Build connectivity maps.
+class Galerkin:
+  #CONSTANTS AND VARIABLES ON THE EXAMPLE WAS GLOBALS1D.
+  #Constructor.       
+  def __init__():
+  #Compute basic Legendre Gauss Lobato grid.
+  def gridLGL(self):
+    Pass
+  #Build reference element matrices.
+  def matrixElements(self):
+    Pass
+  #Create surface integral terms.
+  def surfaceIntegral(self):
+    Pass
+  #Build coordinates of all the nodes.
+  def nodeCoordinates(self):
+    Pass
+  #Calculate geometric factors.
+  def geometricFactors(self):
+    Pass
+  #Compute masks for edge nodes.
+  def masksNodes(self):
+    Pass
+  #Build surface normals and inverse metric at surface.
+  def surfaceNormals(self):
+    Pass
+  #Build connectivity matrix.
+  def connectMatrix(self):
+    Pass
+  #Build connectivity maps.
+  def connectMaps(self):
+    Pass
