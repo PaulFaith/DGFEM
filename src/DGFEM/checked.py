7029ac35f7c0a418679e771e8da953041c9b25ac
PaulFaith <jupafeguz@gmail.com>
2020-11-05 07:18:46 +0000

    StartUp utils done, let's work on the Driver!

--- src/DGFEM/checked.py
+++ src/DGFEM/checked.py
@@ -25,6 +25,25 @@ def jacobi_gauss_lobatto(alpha, beta, n_order):
     
     raise ValueError('n_order must be positive.')
 
+def jacobi_gauss(alpha, beta, n_order):
+    """
+    Compute the order n_order Gauss quadrature points, x, 
+    and weights, w, associated with the Jacobi 
+    polynomial, of type (alpha,beta) > -1 ( <> -0.5).
+    >>> s1 = jacobi_gauss(2,1,0)
+    >>> s2 = [-0.2,  2]
+    >>> np.allclose(s1,s2)
+    True
+    >>> s1 = jacobi_gauss(2,1,1)
+    >>> s2 = [([-0.54691816,  0.26120387]), ([0.76094757, 0.57238576])]
+    >>> np.allclose(s1,s2)
+    True
+    >>> s1 = jacobi_gauss(2,1,2)
+    >>> s2 = [([-0.70882014, -0.13230082,  0.50778763]), ([0.39524241,  0.72312171,  0.21496922])]
+    >>> np.allclose(s1,s2)
+    True
+    """
+
 def mesh_generator(xmin,xmax,k_elem):
     """
     Generate simple equidistant grid with K elements
@@ -188,3 +207,261 @@ def jacobi_polynomial(r, alpha, beta, n_order):
         aold = anew
     
     return jacobi_pol[n_order]
+
+def surface_integral_dg(n_order,vander):
+    """
+    Compute surface integral term in DG formulation
+    >>> r = jacobi_gauss_lobatto(0,0,2)
+    >>> v = vandermonde(2,r)
+    >>> surface_integral_dg(2,v)
+    array([[ 4.5 ,  1.5 ],
+           [-0.75, -0.75],
+           [ 1.5 ,  4.5 ]])
+    >>> r = jacobi_gauss_lobatto(0,0,3)
+    >>> v = vandermonde(3,r)
+    >>> surface_integral_dg(3,v)
+    array([[ 8.        , -2.        ],
+           [-0.89442719,  0.89442719],
+           [ 0.89442719, -0.89442719],
+           [-2.        ,  8.        ]])
+    """
+    # n_faces, n_fp and n_p are defined as global variables
+    n_faces = 1
+    n_fp = 2
+    n_p = n_order+1
+
+    emat = np.zeros([n_p,n_faces*n_fp])
+    emat[0,0] = 1.0
+    emat[n_p-1,1] = 1.0
+
+    v_trans = np.transpose(vander)
+    v_i = np.matmul(v_trans,emat)
+    lift = np.matmul(vander,v_i)
+
+    return lift
+
+def nodes_coordinates(n_order,etov,vx):
+    """
+    Part of StartUp1D.m. Defined to be able to define
+    methods depedent grid properties
+    >>> [Nv,vx,K,etov] = mesh_generator(0,10,4)
+    >>> x = nodes_coordinates(4,etov,vx)
+    >>> x_test = ([[0.00000000,    2.50000000,    5.00000000,    7.50000000], \
+                   [0.43168291,    2.93168291,    5.43168291,    7.93168291], \
+                   [1.25000000,    3.75000000,    6.25000000,    8.75000000], \
+                   [2.06831709,    4.56831709,    7.06831709,    9.56831709], \
+                   [2.50000000,    5.00000000,    7.50000000,   10.00000000]])
+    >>> np.allclose(x,x_test)
+    True
+    """
+
+    jgl = jacobi_gauss_lobatto(0,0,n_order)
+    
+    va = etov[:,0]
+    vb = etov[:,1]
+    vx_va = np.zeros([1,len(va)])
+    vx_vb = np.zeros([1,len(va)])
+    for i in range(len(va)):
+        vx_va[0,i] = vx[va[i]-1]
+        vx_vb[0,i] = vx[vb[i]-1]
+
+    nodes_coord = np.matmul(np.ones([n_order+1,1]),vx_va)+0.5*np.matmul((jgl.reshape(n_order+1,1)+1),(vx_vb-vx_va))
+    return nodes_coord
+
+def geometric_factors(nodes_coord,diff_matrix):
+    """
+    Compute the metric elements for the local mappings of the 1D elements 
+    >>> [Nv,vx,K,etov] = mesh_generator(0,10,4)
+    >>> x = nodes_coordinates(2,etov,vx)
+    >>> r = jacobi_gauss_lobatto(0,0,2)
+    >>> V = vandermonde(2,r)
+    >>> Dr = differentiation_matrix(2,r,V)
+    >>> [rx,J] = geometric_factors(x,Dr)
+    >>> rx_test = ([[0.80000,   0.80000,   0.80000,   0.80000], \
+                    [0.80000,   0.80000,   0.80000,   0.80000], \
+                    [0.80000,   0.80000,   0.80000,   0.80000]])
+    >>> J_test =   ([[1.2500,   1.2500,   1.2500,   1.2500], \
+                     [1.2500,   1.2500,   1.2500,   1.2500], \
+                     [1.2500,   1.2500,   1.2500,   1.2500]])
+    >>> np.allclose(rx,rx_test)
+    True
+    >>> np.allclose(J,J_test)
+    True
+    """
+    xr = np.matmul(diff_matrix,nodes_coord)
+    jacobian = xr
+    rx = 1/jacobian
+
+    return [rx,jacobian]
+
+def connect(etov):
+    """
+    Build global connectivity arrays for 1D grid based on standard 
+    etov input array from grid generator
+    >>> [Nv,vx,K,etov] = mesh_generator(0,10,4)
+    >>> [etoe, etof] = connect(etov)
+    >>> etoe_test =  ([[1,2], \
+                       [1,3], \
+                       [2,4], \
+                       [3,4]])
+    >>> etof_test =  ([[1,1], \
+                       [2,1], \
+                       [2,1], \
+                       [2,2]])
+    >>> np.allclose(etoe,etoe_test)
+    True
+    >>> np.allclose(etof,etof_test)
+    True
+    >>> [Nv,vx,K,etov] = mesh_generator(-1,22,7)
+    >>> [etoe, etof] = connect(etov)
+    >>> etoe_test = ([[1,2],\
+                      [1,3],\
+                      [2,4],\
+                      [3,5],\
+                      [4,6],\
+                      [5,7],\
+                      [6,7]])
+    >>> etof_test = ([[1,1],\
+                      [2,1],\
+                      [2,1],\
+                      [2,1],\
+                      [2,1],\
+                      [2,1],\
+                      [2,2]])
+    >>> np.allclose(etoe,etoe_test)
+    True
+    >>> np.allclose(etof,etof_test)
+    True
+    """
+    n_faces = 2
+    k_elem = np.shape(etov)[0] 
+    total_faces = n_faces*k_elem
+    nv = k_elem+1
+    vn = np.arange(0,2)
+    sp_ftov = np.zeros([total_faces,nv])
+
+    sk = 0
+
+    for i in range(k_elem):
+        for face in range(n_faces):
+            sp_ftov[sk][etov[i][vn[face]]-1] = 1
+            sk += 1
+    
+    sp_ftof = np.matmul(sp_ftov,np.transpose(sp_ftov))-np.identity(total_faces)
+    [faces_2,faces_1] = np.where(sp_ftof==1)
+    #numpy floor returns floats
+
+    element_1 = np.int64(np.floor(faces_1/n_faces))
+    element_2 = np.int64(np.floor(faces_2/n_faces))
+
+    face_1 = np.mod(faces_1,n_faces)
+    face_2 = np.mod(faces_2,n_faces)
+
+    ind = np.arange(len(element_1))
+
+    for i in range(len(element_1)):
+        ind[i] = np.ravel_multi_index((element_1[i],face_1[i]),dims=(k_elem,n_faces))
+
+    etoe_1 = np.transpose(np.arange(1,k_elem+1).reshape(1,k_elem))
+    etoe_2 = np.full([1,n_faces],1)
+    etoe = np.matmul(etoe_1,etoe_2)
+
+    etof_1 = np.full([k_elem,1],1)
+    etof_2 = np.arange(1,n_faces+1).reshape(1,n_faces)
+    etof = np.matmul(etof_1,etof_2)
+
+    for i in range(len(ind)):
+        etoe.ravel()[ind[i]] = element_2[i]+1
+        etof.ravel()[ind[i]] = face_2[i]+1 
+
+    return [etoe, etof]
+
+def normals(k_elem):
+    """
+    Compute outward pointing normals at element faces
+    >>> normals(4)
+    array([[-1., -1., -1., -1.],
+           [ 1.,  1.,  1.,  1.]])
+    """
+    # K is the number of elements, derived from the grid info
+    # n_faces and n_fp are defined as global variables
+    n_faces = 1
+    n_fp = 2
+    nx = np.zeros([n_fp*n_faces,k_elem])
+    nx[0,:] = -1.0
+    nx[1,:] = 1.0
+    return nx 
+
+def build_maps(n_order,nodes_coord,etoe,etof):
+    """
+    Connectivity and boundary tables for nodes given in the K # of elements,
+    each with n_order+1 degrees of freedom.
+    >>> [Nv,vx,K,etov] = mesh_generator(0,10,4)
+    >>> x = nodes_coordinates(4,etov,vx)
+    >>> [etoe, etof] = connect(etov)
+    >>> [vmap_m,vmap_p,vmap_b,map_b] = build_maps(4,x,etoe,etof)
+    >>> vmap_m_test = ([[1,5,6,10,11,15,16,20]])
+    >>> np.allclose(vmap_m,vmap_m_test)
+    True
+    >>> vmap_p_test = ([[1,6,5,11,10,16,15,20]])
+    >>> np.allclose(vmap_p,vmap_p_test)
+    True
+    >>> vmap_b_test = ([[1,20]])
+    >>> np.allclose(vmap_b,vmap_b_test)
+    True
+    >>> map_b_test = ([[1,8]])
+    >>> np.allclose(map_b,map_b_test)
+    True
+    """
+    jgl = jacobi_gauss_lobatto(0,0,n_order)
+    k_elem = np.size(etoe,0)
+    n_p = n_order+1
+    n_faces = 2
+    n_fp = 1
+    #mask defined in globals
+    fmask_1 = np.where(np.abs(jgl+1)<1e-10)[0][0]
+    fmask_2 = np.where(np.abs(jgl-1)<1e-10)[0][0]
+    fmask = [fmask_1,fmask_2]
+
+    node_ids = np.reshape(np.arange(k_elem*n_p),[n_p,k_elem],'F')
+    vmap_m = np.full([k_elem,n_fp,n_faces],0)
+    vmap_p = np.full([k_elem,n_fp,n_faces],0)
+
+    for k1 in range(k_elem):
+        for f1 in range(n_faces):
+            vmap_m[k1,:,f1] = node_ids[fmask[f1],k1]
+
+    for k1 in range(k_elem):
+
+        for f1 in range(n_faces):
+            k2 = etoe[k1,f1]-1
+            f2 = etof[k1,f1]-1
+
+            vid_m = vmap_m[k1,:,f1][0]
+            vid_p = vmap_m[k2,:,f2][0]
+
+            x1 = nodes_coord.ravel('F')[vid_m]
+            x2 = nodes_coord.ravel('F')[vid_p]
+
+            distance = (x2-x1)**2  
+            if (distance < 1e-10):
+                vmap_p[k1,:,f1] = vid_p
+
+    vmap_m+=1
+    vmap_p+=1
+
+    vmap_p = vmap_p.ravel()
+    vmap_m = vmap_m.ravel()
+
+    map_b = np.where(vmap_p==vmap_m)[0]
+    vmap_b = vmap_m[map_b]
+
+    map_b+=1
+    vmap_b
+
+    map_i = 1
+    map_o = k_elem*n_faces
+    vmap_i = 1
+    vmap_0 = k_elem*n_p
+    
+    return [vmap_m,vmap_p,vmap_b,map_b]
