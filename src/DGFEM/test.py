4cb6d342aa40cc89a6a08fae1d0203b351c43400
PaulFaith <jupafeguz@gmail.com>
2020-11-14 06:08:47 +0000

    Startup done!!

--- src/DGFEM/test.py
+++
@@ -1,5 +0,0 @@
-from checked import jacobi_gauss_lobatto
-from scipy import special
-
-r = jacobi_gauss_lobatto(0,0,5)
-print(special.roots_jacobi(4,0,0))
--- src/main.py
+++ src/main.py
@@ -2,23 +2,32 @@ import json
 import argparse
 import os.path
 import sys
-
-from DGFEM.mesh import Mesh
-from DGFEM.startup import Solver
-from DFGEM.advec import 
+from DGFEM.checked import * 
 
 
 #CONSTANTS AND VARIABLES ON THE EXAMPLE WAS GLOBALS1D.
-
+N = 8
 #GENERATE SIMPE MESH.
-
+[Nv, VX, K, EToV] = mesh_generator(0., 2., 10)
 #INITIALIZE SOLVER AND CONSTRUCT GRID AND METRIC.
 
+r = jacobi_gauss_lobatto(0, 0, N)
+V = vandermonde(N, r)
+Dr = differentiation_matrix(N, r, V)
+LIFT = surface_integral_dg(N, V)
+x = nodes_coordinates(N, EToV, VX)
+[rx, J] = geometrix_factors(x, Dr)
+[nx] = normals(K)
+[EToE, EToF] = connect(EToV)
+[vmapM, vmapP, vmapB, mapB] = buidlmaps(N, x, EToE, EToF)
+
 #INITIAL CONDITIONS.
-
+u = np.sin(x)
 #SOLVE PROBLEM.
-
-#EXPORT DATA.
+for finaltime in range(5):
+  [sol] = advec(u, finaltime)
+  
+  #EXPORT DATA.
 
 #PLOT SOLUTION.
 
